e4323888d7cc05592c1cf5381468ac09933c794c
Nihat ÖZKURT <nihat_ozkurt@hotmail.com>
2024-02-28 15:38:06 +0300

    FULLCALENDAR-TAKVIM

---
+++ FullCalendar/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FullCalendar.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index2(string kullanici,string sifre)
+        {
+            if (kullanici=="admin" && sifre=="123456") { return Redirect("/Menu/Index"); }
+            
+            
+            else { return Redirect("/Login/Index"); }
+
+        }
+
+    }
+}
